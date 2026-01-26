ae5636329bc1f312b15334d41205dca53ca8c0be
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2026-01-26 16:02:50 +0100

    msg

--- middlewares/email.js
+++ middlewares/email.js
@@ -1,47 +1,3 @@
-// const nodemailer = require("nodemailer");
-// const fs = require("fs");
-// const { promisify } = require("util");
-
-// const readFile = promisify(fs.readFile);
-
-// const email = async (receiver, subject, body, replacements = {}) => {
-//   try {
-//     let transporter = nodemailer.createTransport({
-//       host: "mail.merrymemorries.com",
-//       port: 465,
-//       secure: false,
-//       auth: {
-//         user: "support@merrymemorries.com",
-//         pass: "ZQor41rI7qIdh",
-//       },
-//     });
-
-//     // Read HTML template
-//     let html = await readFile(`html_mails/${body}`, "utf8");
-
-//     // Replace all placeholders dynamically
-//     for (let key in replacements) {
-//       const placeholder = new RegExp(`{{${key}}}`, "g");
-//       html = html.replace(placeholder, replacements[key]);
-//     }
-
-//     // Send email
-//     let info = await transporter.sendMail({
-//       from: '"Property Listing API" <support@merrymemorries.com>',
-//       to: receiver,
-//       subject: subject,
-//       html: html,
-//     });
-
-//     console.log("Email sent:", info.messageId);
-//   } catch (err) {
-//     console.error("Email Error:", err);
-//     throw err;
-//   }
-// };
-
-// module.exports = email;
-
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const { promisify } = require("util");
@@ -50,38 +6,31 @@ const readFile = promisify(fs.readFile);
 
 const email = async (receiver, subject, body, replacements = {}) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "mail.merrymemorries.com",
-      port: 587,
-      secure: false, // MUST be false for 587
+    let transporter = nodemailer.createTransport({
+      host: "mail.universaltrustlogistiks.com",
+      port: 465,
+      secure: false,
       auth: {
-        user: "support@merrymemorries.com",
-        pass: process.env.EMAIL_PASS, // move to env
+        user: "support@universaltrustlogistiks.com",
+        pass: "ZOdt227SZxjHy",
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     });
 
-    // Verify connection
-    await transporter.verify();
-    console.log("SMTP connected successfully");
-
+    // Read HTML template
     let html = await readFile(`html_mails/${body}`, "utf8");
 
+    // Replace all placeholders dynamically
     for (let key in replacements) {
       const placeholder = new RegExp(`{{${key}}}`, "g");
       html = html.replace(placeholder, replacements[key]);
     }
 
+    // Send email
     let info = await transporter.sendMail({
-      from: '"Property Listing API" <support@merrymemorries.com>',
+      from: '"Property Listing API" <support@universaltrustlogistiks.com>',
       to: receiver,
-      subject,
-      html,
+      subject: subject,
+      html: html,
     });
 
     console.log("Email sent:", info.messageId);
