671af53a1a1549cc862536900b8f13316780ed88
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2026-04-09 23:19:25 +0100

    msg

--- controllers/user.js
+++ controllers/user.js
@@ -92,12 +92,12 @@ changeProfilePicture = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    if (!req.file) {
+    if (!req.files) {
       return res.status(400).json({ message: "Profile picture is required" });
     }
 
     // Upload the new image (returns the URL or path)
-    const imageUrl = await uploadImages(req.file, "profile_pictures");
+    const imageUrl = await uploadImages(req.files, "profile_pictures");
 
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found" });
